22e8fea393e3be979a1255a289674e2b10f25829
Thanh Tung <thanhtungphankt@gmail.com>
2023-10-04 01:10:12 +0700

    demo commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+<><script src="js/jquery.slim.min.js">
+    </script><script src="js/popper.min.js"></script><script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+    <script>
+    $(document).ready(function(){$('[data-toggle="tooltip"]').tooltip()};
+    {"}"});
+</script>,
+<script>
+        $("#carouselButton").click(function(){ }
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {$("#mycarousel").carousel('pause')};
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+        {"}"}
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){$("#mycarousel").carousel('cycle')};
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');
+        {"}"}
+        {"}"});
+
+
+    </script></>
